5434fd75730285413e49b9027c57d79cb12b9b10
TheolaAshu <ashutheola@gmail.com>
2023-09-12 09:36:25 -0700

    last advance task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point to print a quote
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
