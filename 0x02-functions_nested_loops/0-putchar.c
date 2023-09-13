4ba7463be6e2dc59278da86cf5c3057d8ff12b51
TheolaAshu <ashutheola@gmail.com>
2023-09-13 00:22:14 -0700

    made putchar .c to be betty compliant

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  * Return: Always 0 Success
  */
 int main(void)
-  {
+{
 	  _putchar("_");
 	  _putchar("p");
 	  _putchar("u");
@@ -14,4 +14,4 @@ int main(void)
 	  _putchar("a");
 	  _putchar("r");
 	  _putchar('/n');
-  }
+}
